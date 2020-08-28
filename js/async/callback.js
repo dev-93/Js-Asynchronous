ce1a60478be611d1cedac20f85b36efaa98e5dd1
dev-93 <cjb940103@gmail.com>
2020-08-29 03:14:42 +0900

    arrow function

--- js/async/callback.js
+++ js/async/callback.js
@@ -7,10 +7,9 @@
 // callback함수 전달해준 함수를 나중에 불러줘라는 개념
 
 console.log("1");
-setTimeout(function () {
-    console.log("2");
-}, 1000);
+setTimeout(() => console.log("2"), 1000);
 console.log("3");
 
 // 자바스크립트 엔진은 제일 위부터 아래로 실행
 // setTimeout은 브라우저 API니까 브라우저에게 callback 실행해달라고 요청
+// 지금 당장 실행하지 않고, 1초가 지나고 함수 불러줘
