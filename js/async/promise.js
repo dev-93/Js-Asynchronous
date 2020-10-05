9fc4b10cf5812675e20d7a9505cad15b77fb3cdf
dev-93 <cr000@naver.com>
2020-10-06 00:30:17 +0900

    consumer만들라고 ㅋ

--- js/async/promise.js
+++ js/async/promise.js
@@ -10,6 +10,16 @@
 const promise = new Promise((resolve, reject) => {
     console.log("doing something ...");
     setTimeout(() => {
-        resolve("taenam");
+        // resolve("taenam");
+        reject(new Error("no network"));
     }, 2000);
 });
+
+// 2. Consumers: then catch, finally
+promise
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
