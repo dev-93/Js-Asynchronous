2c75619d4199449dbafe745cca82173997fdef83
dev-93 <cr000@naver.com>
2020-10-06 00:09:40 +0900

    userStorage 생성

--- js/async/callback.js
+++ js/async/callback.js
@@ -57,3 +57,28 @@ class UserStorage {
         }, 1000);
     }
 }
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+
+userStorage.loginUser(
+    id,
+    password,
+    (user) => {
+        userStorage.getRoles(
+            user,
+            (userWithRole) => {
+                alert(
+                    `hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+                );
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    },
+    (error) => {
+        console.log(error);
+    }
+);
