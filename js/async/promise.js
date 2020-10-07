357965265ba7acc650832212b45545fb282ef617
dev-93 <cr000@naver.com>
2020-10-07 23:37:14 +0900

    Error Hanldring setting

--- js/async/promise.js
+++ js/async/promise.js
@@ -28,14 +28,30 @@ promise
 // 3. Promise chaining
 const fetchNumber = new Promise((resolve, reject) => {
     setTimeout(() => resolve(1), 1000);
-})
+});
 
 fetchNumber
-    .then(num => num * 2)
-    .then(num => num * 3)
-    .then(num => {
+    .then((num) => num * 2)
+    .then((num) => num * 3)
+    .then((num) => {
         return new Promise((resolve, reject) => {
             setTimeout(() => resolve(num - 1), 1000);
         });
     })
-    .then(num => console.log(num));
+    .then((num) => console.log(num));
+
+// 4.Error Handling
+const getHen = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve("chicken"), 1000);
+    });
+
+const getEgg = (hen) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${hen} => egg`), 1000);
+    });
+
+const getFri = (egg) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${egg} => fri`), 1000);
+    });
