d8e2fbc333857ca26f6cc8e7479b8f57161b97fe
dev-93 <cr000@naver.com>
2020-10-23 23:55:03 +0900

    await 사용

--- js/async/async.js
+++ js/async/async.js
@@ -12,3 +12,27 @@ async function fetchUser() {
 const user = fetchUser();
 console.log(user);
 
+// 2. await
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+    await delay(1000);
+    return '🍎'
+}
+
+async function getBanana() {
+    await delay(1000);
+    return '🍌'
+}
+
+async function pickFruits() {
+    const apple = await getApple();
+    const banana = await getBanana();
+
+    return `${apple} + ${banana}`;
+}
+
+pickFruits().then(console.log);
